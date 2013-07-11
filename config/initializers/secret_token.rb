aefe49724892b6858946e0d0d71449ebe0d64ffb
James Page <jpage@reckloose.com>
2013-07-11 19:25:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoviesApp::Application.config.secret_key_base = '35885e7498b95bc09d891b9d7b75952a0757621f31aafc97c9ea2334309ff47435fcbe5d60f25d31ce2b9dd32ddf59f13e8234ede5f6ff369b02afbedce04950'
